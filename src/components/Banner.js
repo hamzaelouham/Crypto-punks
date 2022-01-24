7972d50aebf2b734a0bcbdb72d1914533a236b16
hamzaelouham <hamzaelouham@gmail.com>
2022-01-24 12:47:52 +0100

    finsh header

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Banner() {
+  return (
+    <div>
+      <div>helllo this Banner</div>
+    </div>
+  );
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,35 @@
 import React from "react";
+import logo from "../assets/cryptopunk-logo.png";
+import searchIcon from "../assets/search.png";
+import theme from "../assets/theme-switch.png";
 
 export default function Header() {
   return (
-    <div>
-      <h2>header !</h2>
-    </div>
+    <header className="header">
+      <div className="logocontainer">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <div className="searchbar">
+        <div className="searchicon">
+          <img src={searchIcon} alt="searchicon" />
+        </div>
+        <input
+          type="text"
+          className="searchinput"
+          placeholder="collection , items or user..."
+        />
+      </div>
+      <div className="headeritems">
+        <p>Create</p>
+        <p>Marketpace</p>
+        <p>Drops</p>
+      </div>
+      <div className="actions">
+        <div className="themeswitch">
+          <img src={theme} alt="theme" />
+        </div>
+        <button className="gotin"> GOT IN</button>
+      </div>
+    </header>
   );
 }
